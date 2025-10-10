9e8984e0ca8d656e4a06f97b1e71a05eeb21ac7a
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2025-10-10 23:46:32 +0900

    [fix] supabase

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { Button } from "flowbite-react";
+import SignUp from "./SignUp";
 
 export default function Header() {
   return (
-    <Button
-      type="button"
-      className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-    >
-      Header
-    </Button>
+    <>
+      <Button
+        type="button"
+        className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      >
+        Header
+      </Button>
+      <SignUp></SignUp>
+    </>
   );
 }
