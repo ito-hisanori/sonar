00fba9be9abaed765b46edd712a884d3d745387a
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2025-12-16 08:32:16 +0900

    add sidebar

--- src/layout-provider/header.tsx
+++ src/layout-provider/header.tsx
@@ -1,8 +1,11 @@
 import usersGlobalStore, { UsersGlobalStore } from "@/global-store/users-store";
+import { Menu } from "lucide-react";
 import React from "react";
+import SidebarMenuItems from "./sidebar-menuitems";
 
 function Header() {
   const { user } = usersGlobalStore() as UsersGlobalStore;
+  const [openSidebar, setOpenSidebar] = React.useState(false);
   return (
     <div className="p-6 flex justify-between bg-primary items-center">
       <div>
@@ -10,7 +13,19 @@ function Header() {
       </div>
       <div className="flex items-center gap-5">
         <h1 className="text-white text-sm">{user?.first_name}</h1>
+        <Menu
+          size={14}
+          onClick={() => setOpenSidebar(true)}
+          className="text-white cursor-pointer"
+        />
       </div>
+
+      {openSidebar && (
+        <SidebarMenuItems
+          openSidebar={openSidebar}
+          setOpenSidebar={setOpenSidebar}
+        />
+      )}
     </div>
   );
 }
---
+++ src/layout-provider/sidebar-menuitems.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { title } from "process";
+import { Icon, LayoutDashboard, ListChecks, LogOut } from "lucide-react";
+import usersGlobalStore, { UsersGlobalStore } from "@/global-store/users-store";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Cookies from "js-cookie";
+import toast from "react-hot-toast";
+
+function SidebarMenuItems({
+  openSidebar,
+  setOpenSidebar,
+}: {
+  openSidebar: boolean;
+  setOpenSidebar: (open: boolean) => void;
+}) {
+  const { user } = usersGlobalStore() as UsersGlobalStore;
+  const iconSize = 14;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const logoutHandler = () => {
+    toast.success("Logged out successfully");
+    Cookies.remove("jwt_token");
+    // Cookies.remove("role");
+    router.push("/?formType=login");
+  };
+  const userMenuItems: any = [
+    {
+      title: "Dashboard",
+      icon: <LayoutDashboard size={iconSize} />,
+      path: "/user/dashboard",
+    },
+    {
+      title: "Dive Log",
+      icon: <ListChecks size={iconSize} />,
+      path: "/user/divelog",
+    },
+  ];
+  // const adminMenuItems = [];
+
+  // const menuItems = user?.role === "admin" ? adminMenuItems : userMenuItems;
+
+  return (
+    <Sheet open={openSidebar} onOpenChange={setOpenSidebar}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle></SheetTitle>
+        </SheetHeader>
+
+        <div className="flex flex-col gap-12 mt-14 px-7">
+          {userMenuItems.map((item: any, index: number) => (
+            <div
+              key={index}
+              className={`p-3 flex gap-3 items-center cursor-pointer ${
+                pathname === item.path
+                  ? "bg-gray-100 border border-gray-500 text-primary rounded-lg"
+                  : ""
+              }`}
+              onClick={() => {
+                setOpenSidebar(false);
+                router.push(item.path);
+              }}
+            >
+              {item.icon}
+              <h1 className="text-sm">{item.title}</h1>
+            </div>
+          ))}
+
+          <Button onClick={logoutHandler}>
+            <LogOut /> Logout
+          </Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default SidebarMenuItems;
