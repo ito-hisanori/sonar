46cdc2df324366cd9fe3b7670e811b77f740bd70
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2025-12-21 23:24:47 +0900

    fix time

--- src/helpers/date-time-formats.ts
+++ src/helpers/date-time-formats.ts
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
 
 export const getDateTimeFormat = (date: Date | string) => {
-  return dayjs(date).format("YYYY-MM-DD HH:mm");
+  return dayjs(date).format("YYYY-MM-DDTHH:mm");
 };
